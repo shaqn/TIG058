c6aca88db700d09ec871a72c85c94815e767da53
annanumat <anna_anumat@hotmail.com>
2015-02-18 12:17:17 +0000

    Ordning och kommentarer i menu

--- club/Menu.java
+++ club/Menu.java
@@ -28,26 +28,43 @@ public class Menu {
 			public void menuItemSelected(ConsoleMenuEvent e) { 
 			    ch.printMembersAlphaFirstName(); 
 			} 
-		},"List members by first name" );
+		},"List members by first name" ); //Aternativ 1= Medlemmar i alfabetiskt ordning förnamn
 
 		menu.addMenuItem(new MenuItem(){
 			public void menuItemSelected(ConsoleMenuEvent e) { 
 			    ch.printMembersAlpha(); 
 			} 
-		},"List members by Surname" );  //Behöver fixas så det blir ordning på efternamn
+		},"List members by Surname" );  //Aternativ 2= Medlemmar i alfabetiskt ordning efternamn
 
 		menu.addMenuItem(new MenuItem(){
 			public void menuItemSelected(ConsoleMenuEvent e) { 
 			    ch.printMembers(); 
 			} 
 
-		},"List members in order of ID" );
+		},"List members in order of ID" ); //Alternativ 3= Medlemmar efter ID
 		
 		menu.addMenuItem(new MenuItem(){
 			public void menuItemSelected(ConsoleMenuEvent e) { 
 			    ch.printTeams(); 
 			} 
-		},"List members in order of Team" );
+		},"List members in order of Team" ); //Alternatv 4= Lista medlemmar i ordning efter lag
+
+menu.addMenuItem(new MenuItem(){
+			public void menuItemSelected(ConsoleMenuEvent e) { 
+			    ch.printTeams(); 
+			} 
+		},"List parents in a team" ); //Alternativ 6= Lista alla föräldrar i  ett lag
+
+menu.addMenuItem(new MenuItem(){
+			public void menuItemSelected(ConsoleMenuEvent e) { 
+			    ch.printTeams(); 
+			} 
+		},"List e-mails for a member " ); //Alternativ 7= Lista e-post adresser -> om medlemmen skall e-post användas, om inte skall föräldrarnas e-post användas
+
+
+
+
+
 
 		menu.addMenuItem(new MenuItem(){
 			public void menuItemSelected(ConsoleMenuEvent e) { 
@@ -55,7 +72,7 @@ public class Menu {
 			    String search = "%" + input.trim() + "%";
 			    ch.printMembers(search); 
 			}
-		},"List members in order of " );
+		},"List members in a team " ); //Alternativ 5=Lista alla medlemmar i ett lag
 
 		menu.addMenuItem(new MenuItem(){
 			public void menuItemSelected(ConsoleMenuEvent e) { 
@@ -69,3 +86,4 @@ public class Menu {
     }
 
 }
+
