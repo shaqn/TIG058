b6986faae411797f8ed1c77a35a287c51b2d9879
annanumat <anna_anumat@hotmail.com>
2015-02-19 14:04:31 +0000

    Javadoc menu färdig

--- club/Menu.java
+++ club/Menu.java
@@ -1,9 +1,10 @@
 package club;
 
 import com.sandklef.edu.ConsoleMenu.*;
+
 /**
 * A class that contains options for the menu 
-*
+* Returns calls (anrop?) from ClubHelper 
 *
 */
 public class Menu {
@@ -26,8 +27,8 @@ public class Menu {
 			} 
 		}," " );
 /**
-*Aternativ 1= Medlemmar i alfabetiskt ordning förnamn
-*
+* Aternative 1= Members in alphabetic order by first name
+* calls for the method  printMembersAlphaFirstName() in ClubHelper
 */
 	   	menu.addMenuItem(new MenuItem(){
 			public void menuItemSelected(ConsoleMenuEvent e) { 
@@ -35,8 +36,8 @@ public class Menu {
 			} 
 		},"List Members in order of First name" ); 
 /**
-*Aternativ 2= Medlemmar i alfabetiskt ordning efternamn
-*
+*Aternative 2= Members in alphabetic order by surname
+*calls for the method  printMembersAlpha() in ClubHelper
 */
 		menu.addMenuItem(new MenuItem(){
 			public void menuItemSelected(ConsoleMenuEvent e) { 
@@ -44,7 +45,8 @@ public class Menu {
 			} 
 		},"List Members in order of Surname" );  
 
-/**Alternativ 3= Medlemmar efter ID
+/**Alternative 3= Members in order by ID
+*calls for the method  printID() in ClubHelper
 *
 */
 		menu.addMenuItem(new MenuItem(){
@@ -54,9 +56,11 @@ public class Menu {
 
 		},"List Members in order of ID" ); 
 		
-/*Alternatv 4= Lista medlemmar i ordning efter lag		
+/*Alternative 4= Members in order by Teams	
+*calls for the method printTeams() in ClubHelper
 *
-*/		menu.addMenuItem(new MenuItem(){
+*/		
+		menu.addMenuItem(new MenuItem(){
 			public void menuItemSelected(ConsoleMenuEvent e) { 
 			    ch.printTeams();
 			    
@@ -65,6 +69,7 @@ public class Menu {
 
 
 /*Alternatv 4= Lista medlemmar i ordning efter lag
+*calls for the method  printTeamMembers() in ClubHelper
 *
 */
 		menu.addMenuItem(new MenuItem(){
@@ -75,7 +80,8 @@ public class Menu {
 		},"List Members of a Team" ); 
 
 /*
-*Alternativ 6= Lista alla föräldrar i  ett lag
+* Alternativ 6= Lista alla föräldrar i  ett lag
+* calls for the method  printParents() in ClubHelper
 */
 		menu.addMenuItem(new MenuItem(){
 			public void menuItemSelected(ConsoleMenuEvent e) { 
@@ -85,7 +91,7 @@ public class Menu {
 		},"List Parents in a Team" ); 
 
 /*Alternativ 7= Lista e-post adresser -> om medlemmen skall e-post användas, om inte skall föräldrarnas e-post användas
-*
+*calls for the method emailMember() in ClubHelper
 */
 		menu.addMenuItem(new MenuItem(){
 			public void menuItemSelected(ConsoleMenuEvent e) { 
