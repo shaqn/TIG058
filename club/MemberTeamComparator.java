9e4eeb1083472e4edfc27ee1df7e89541f146018
Zakiaf <kia_fazzi@hotmail.com>
2015-02-19 14:18:43 +0100

    Kommentarer på Comparators

--- club/MemberTeamComparator.java
+++ club/MemberTeamComparator.java
@@ -3,8 +3,8 @@ package club;
 import tig058.handin01.member.Member;
 import java.util.*;
 /**
- * Simple compator class intented to be used when wanting to Sort
- * lists of members according to team. Sorts primarily on members of team and
+ * Simple compator class intenteded to be used when wanting to Sort Teams
+ * lists members according to team. Sorts primarily on members of team and
  * secondaily on given name.
  *
  */
