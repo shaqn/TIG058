cdba60913e6d7effa5a502a531a9f2bd9a913477
Zakiaf <kia_fazzi@hotmail.com>
2015-02-19 22:22:11 +0100

    Memberteamcomparator commented

--- club/MemberTeamComparator.java
+++ club/MemberTeamComparator.java
@@ -3,8 +3,8 @@ package club;
 import tig058.handin01.member.Member;
 import java.util.*;
 /**
- * Simple compator class intenteded to be used when wanting to Sort Teams
- * lists members according to team. Sorts primarily on members of team and
+ * Simple comparator which is intenteded to be used when wanting to sort teams.
+ * Lists members according to team. Sorts primarily on members of team and
  * secondaily on given name.
  *
  */
